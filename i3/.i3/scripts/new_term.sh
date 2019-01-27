350533fa61fbe4da54a2f7c8950adc0966737641
Fred Bunt <fredrick.bunt@gmail.com>
2019-01-27 07:24:05 -0700

    Switch to alacritty as preferred term and set no id on launch

--- i3/.i3/scripts/new_term.sh
+++ i3/.i3/scripts/new_term.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-konsole --workdir "$(~/bin/lastcwd)" &
+
+# Default to konsole
+term=konsole
+flag="--workdir"
+args=
+cwd=
+# Use alacritty if it is present
+if [ -x "$(command -v alacritty)" ]; then
+    term=alacritty
+    flag="--working-directory"
+fi
+if [ -x "$(command -v lastcwd)" ]; then
+    cwd="$(lastcwd)"
+    if [ -n "$cwd" ]; then
+        # Only set args if cwd could be determined
+        args="$flag $cwd"
+    fi
+fi
+$term $args &
