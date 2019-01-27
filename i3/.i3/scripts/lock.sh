0858593908915cbf98f0cbf8e0ce66bd9ebd5bea
Fred Bunt <fredrick.bunt@gmail.com>
2019-01-26 20:22:48 -0700

    Add proper local caching and stop using git for lock imgs

--- i3/.i3/scripts/lock.sh
+++ i3/.i3/scripts/lock.sh
@@ -12,17 +12,35 @@
 #   | \/ |       ---| \/ |---
 #   ------          ------
 
-lock_dir="$HOME/.i3/lock"
-full_img="$lock_dir/lock_full.png"
-img_out="$lock_dir/lock"
+wallpaper="$HOME/.i3/wallpaper/wallpaper.png"
+lock_cache="$HOME/.cache/lock_cache"
+full_img="$lock_cache/wp_blur.png"
+blur="0x40"
+# Final image used by i3lock
+img_out="$lock_cache/lock.png"
 # Input image names for convert
 screen_imgs=
+
+if [ ! -d "$lock_cache" ]; then
+    mkdir -p "$lock_cache"
+fi
+# Create blurred version of wallpaper to use as lock screen
+if [ ! -f "$full_img" ]; then
+    if [ -f "$wallpaper" ]; then
+        convert "$wallpaper" -blur "$blur" "$full_img"
+    else
+        # No wallpaper found so just use default i3lock
+        /usr/bin/i3lock
+        exit
+    fi
+fi
+
 # For each screen resolution, create an image to fit it
-while read line; do
-    res=$(echo $line | awk '{print $1}')
+while read -r line; do
+    res=$(echo "$line" | awk '{print $1}')
 
     # Create image for the screen
-    screen_img="$lock_dir/lock_${res}.png"
+    screen_img="$lock_cache/lock_${res}.png"
     if [ ! -f "$screen_img" ]; then
         # Resize and crop the image to fit the resolution and add prompt text
         convert "$full_img" -resize "${res}^" -gravity center -extent "$res" \
@@ -30,12 +48,9 @@ while read line; do
             "What's the password?" "$screen_img"
     fi
     screen_imgs="$screen_imgs ${screen_img}"
-    img_out="${img_out}_${res}"
-done <<< "$(xrandr | grep \*)"
-# Final image name
-img_out="${img_out}.png"
+done <<< "$(xrandr | grep "\*")"
 # Create the final image if it doesn't already exist
 if [ ! -f "$img_out" ]; then
-    convert $screen_imgs +append $img_out
+    convert $screen_imgs +append "$img_out"
 fi
-/usr/bin/i3lock -i $img_out -t
+/usr/bin/i3lock -i "$img_out" -t
