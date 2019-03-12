d9254e11c8ef3da9dd4f7e4089b97eeca871f471
Fred <fred@fbunt-ws.cs.umt.edu>
2019-03-12 14:54:03 -0600

    Add ubuntu setup scripts

---
+++ setup/ubuntu/ubuntu-system-setup.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+# Nvidia drivers
+add-apt-repository ppa:graphics-drivers/ppa
+apt update
+apt install -y "$(ubuntu-drivers list | sort | tail -n 1)"
+
+echo "Installing programs and dependencies"
+# Programs and libs
+apt install -y \
+    arandr \
+    asciidoc \
+    autoconf \
+    automake \
+    clang \
+    cmake \
+    cmake-data \
+    cowsay \
+    dmenu \
+    feh \
+    git \
+    gparted \
+    i3lock \
+    i3status \
+    imagemagick \
+    lm-sensors \
+    make \
+    ncdu \
+    nco \
+    neovim \
+    nfs-common \
+    numlockx \
+    openjdk-11-jdk \
+    pkg-config \
+    ripgrep \
+    rofi \
+    shellcheck \
+    snapd \
+    stow \
+    vim \
+    vlc \
+    xclip \
+    zsh \
+    build-essential \
+    libasound2-dev \
+    libcairo2-dev \
+    libcogl-pango-dev \
+    libconfig-dev \
+    libcurl4-gnutls-dev \
+    libdbusmenu-glib-dev \
+    libev-dev \
+    libfontconfig1-dev \
+    libfreetype6-dev \
+    libgl1-mesa-dev \
+    libjsoncpp-dev \
+    libmpdclient-dev \
+    libnl-3-dev \
+    libnl-genl-3-dev \
+    libpango1.0-dev \
+    libpcre3-dev \
+    libpulse-dev \
+    libstartup-notification0-dev \
+    libx11-dev \
+    libxcb-composite0-dev \
+    libxcb-cursor-dev \
+    libxcb-ewmh-dev \
+    libxcb-icccm4-dev \
+    libxcb-image0-dev \
+    libxcb-keysyms1-dev \
+    libxcb-randr0-dev \
+    libxcb-shape0-dev \
+    libxcb-util0-dev \
+    libxcb-xinerama0-dev \
+    libxcb-xkb-dev \
+    libxcb-xrm-dev \
+    libxcb-xrm0 \
+    libxcb1-dev \
+    libxcomposite-dev \
+    libxdamage-dev \
+    libxfixes-dev \
+    libxinerama-dev \
+    libxkbcommon-dev \
+    libxkbcommon-x11-dev \
+    libxrandr-dev \
+    libyajl-dev \
+    libyajl-dev \
+    ncurses-dev \
+    python-xcbgen \
+    wireless-tools \
+    xcb-proto
+
+ln -s /lib/x86_64-linux-gnu/libpcre.so.3 /lib/x86_64-linux-gnu/libpcre.so.1
---
+++ setup/ubuntu/ubuntu-user-setup.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+echo "Installing rust"
+curl https://sh.rustup.rs -sSf | sh
+
+cargo install fd-find
+
+# git repos needed
+echo "Cloning git repos"
+mkdir -p "$HOME/repos"
+cd "$HOME/repos"
+
+declare -a repos=(
+    https://github.com/jwilm/alacritty.git
+    https://github.com/rupa/z.git
+    https://github.com/stark/siji.git
+    https://github.com/tryone144/compton.git
+    https://github.com/wknapik/lastcwd.git
+    https://www.github.com/Airblader/i3.git
+    https://github.com/Airblader/i3blocks-gaps.git
+)
+
+for r in "${repos[@]}"; do
+    git clone "$r"
+done
+mv i3 i3-gaps
+wget https://github.com/jaagr/polybar/releases/download/3.3.0/polybar-3.3.0.tar
+tar -xvf polybar-*.tar
+
+
+# Install fonts
+echo "Installing fonts"
+declare -a inconsolata_files=(
+    https://github.com/DeLaGuardo/Inconsolata-LGC/raw/master/inconsolatalgc.ttf
+    https://github.com/DeLaGuardo/Inconsolata-LGC/raw/master/inconsolatalgcbold.ttf
+    https://github.com/DeLaGuardo/Inconsolata-LGC/raw/master/inconsolatalgcbolditalic.ttf
+    https://github.com/DeLaGuardo/Inconsolata-LGC/raw/master/inconsolatalgcitalic.ttf
+)
+
+echo "Installing Inconsolata"
+font_dir="$HOME/.local/share/fonts/inconsolata-lgc"
+mkdir -p "$font_dir"
+for url in "${inconsolata_files[@]}"; do
+    wget --directory-prefix="$font_dir"  "$url"
+done
+fc-cache -fv
+
+
+echo "Fetching anaconda installer"
+cd $HOME/Downloads
+wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
+
+
+echo "Installing gotop"
+cd $HOME
+mkdir -p $HOME/bin
+git clone --depth 1 https://github.com/cjbassi/gotop /tmp/gotop
+/tmp/gotop/scripts/download.sh
+mv ./gotop $HOME/bin
