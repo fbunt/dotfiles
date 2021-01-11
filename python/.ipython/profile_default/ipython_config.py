cd37eec391971eae329fd22f111eac09e6a45ec8
Fred <fredrick.bunt@gmail.com>
2021-01-11 14:01:06 -0700

    Update history lengths

--- python/.ipython/profile_default/ipython_config.py
+++ python/.ipython/profile_default/ipython_config.py
@@ -203,11 +203,11 @@
 #c.InteractiveShell.enable_html_pager = False
 
 ## Total length of command history
-#c.InteractiveShell.history_length = 10000
+c.InteractiveShell.history_length = 10000
 
 ## The number of saved history entries to be loaded into the history buffer at
 #  startup.
-#c.InteractiveShell.history_load_length = 1000
+c.InteractiveShell.history_load_length = 5000
 
 ##
 #c.InteractiveShell.ipython_dir = ''
