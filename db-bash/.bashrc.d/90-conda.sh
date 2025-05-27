2b72af00e42e918650d8040bcd2f9be6f0d8efb9
Fred Bunt <fredrick.bunt@gmail.com>
2025-05-27 12:11:27 -0600

    Add new mamba init

--- db-bash/.bashrc.d/90-conda.sh
+++ db-bash/.bashrc.d/90-conda.sh
@@ -18,3 +18,16 @@ fi
 # <<< conda initialize <<<
 
 conda activate main
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+export MAMBA_EXE="${HOME}/anaconda3/bin/mamba";
+export MAMBA_ROOT_PREFIX="${HOME}/anaconda3";
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
