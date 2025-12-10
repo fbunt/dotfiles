084c5d58f6fdb386897762e2eb2baeb7454524f2
Fred Bunt <fredrick.bunt@gmail.com>
2025-12-10 16:36:01 -0700

    Actually add stylua-nvim

--- nvim/.config/nvim/lua/plugins/misc-langs.lua
+++ nvim/.config/nvim/lua/plugins/misc-langs.lua
@@ -1,7 +1,8 @@
 --# selene: allow(mixed_table)
 return {
-    -- Web
     { "rhysd/vim-clang-format", ft = "cpp" },
+    { "ckipp01/stylua-nvim" },
+    -- Web
     { "elzr/vim-json", ft = "json" },
     { "hail2u/vim-css3-syntax", ft = { "css", "html" } },
     { "leafgarland/typescript-vim", ft = "typescript" },
